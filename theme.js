2dd7f879f5993e7a7984b972cc478bcafca4d7ed
paulohrsodre <paulohrsodre@hotmail.com>
2024-03-12 22:42:27 -0300

    Ajustes na paleta de cores do modo escuro

--- theme.js
+++ theme.js
@@ -1,10 +1,18 @@
 const checkbox = document.getElementById('tema-escuro');
 const logo = document.getElementById('logo');
+const info = document.getElementById('info');
+const imagem = document.getElementById('resultado_imagem')
 const body = document.body;
 const root = document.documentElement;
 
 const lightLogo = './assets/Logo.svg'; // Path to your light mode logo
 const darkLogo = './assets/Logo-dm.svg'; // Path to your dark mode logo
+
+const lightInfo = './assets/info.svg';
+const darkInfo = './assets/info-dm.svg';
+
+const lightImagem = './assets/character.svg';
+const darkImagem = './assets/character-dm.svg';
 
 const lightMode = {
   '--cor-principal': '#0A3871', // Main color (light mode)
@@ -34,4 +42,6 @@ checkbox.addEventListener('change', () => {
   }
   body.classList.toggle('tema-escuro');
   logo.src = currentTheme === darkMode ? darkLogo : lightLogo;
+  info.src = currentTheme === darkMode ? darkInfo : lightInfo;
+  imagem.src = currentTheme === darkMode ? darkImagem : lightImagem;
 });
